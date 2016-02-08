5b46556e6066950c14af539f2e80fab450c83c52
Jiro Iwamoto <jirokun@gmail.com>
2016-02-08 23:39:07 +0900

    不要なファイルの削除

--- editor/constants.js
+++
@@ -1,2 +0,0 @@
-export const CHANGE_DEFS = 'CHANGE_DEFS';
-export const FLOW_SELECT = 'FLOW_SELECT';
--- runtime/constants.js
+++
@@ -1,5 +0,0 @@
-export const CHANGE_DEFS = 'CHANGE_DEFS';
-export const NEXT_PAGE = 'NEXT_PAGE';
-export const PREV_PAGE = 'PREV_PAGE';
-export const VALUE_CHANGE = 'VALUE_CHANGE';
-export const FLOW_SELECT = 'FLOW_SELECT';
