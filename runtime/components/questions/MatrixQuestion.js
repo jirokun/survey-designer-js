695408ce417f2c77c1e9832af2f1ce078c5f5fc8
Jiro Iwamoto <jirokun@gmail.com>
2016-04-16 22:18:30 +0900

    matrixの追加

--- runtime/components/questions/MatrixQuestion.js
+++ runtime/components/questions/MatrixQuestion.js
@@ -25,23 +25,86 @@ export default class MatrixQuestion extends Component {
       </label>;
     });
   }
+  isHorizontal() {
+    const { direction } = this.props;
+    if (direction === 'horizontal') {
+      return true;
+    } else if (direction === 'vertical') {
+      return false;
+    } else {
+      throw 'illegal direction: ' + direction;
+    }
+  }
   renderhHeader() {
-    const { vChoices, hChoices } = this.props;
-    return <thead><tr><td/>{hChoices.map(c => <td>{c}</td>)}</tr></thead>;
+    const { questions, values, direction } = this.props;
+    const labels = this.isHorizontal() ? values : questions;
+    return <thead><tr><td/>{labels.map(c => {
+      if (Utils.isString(c)) {
+        return <td>{c}</td>;
+      } else {
+        return <td>{c.label}</td>;
+      }
+    })}</tr></thead>;
+  }
+  renderCell() {
+    const { cellType } = this.props;
+    switch (cellType) {
+      case 'checkbox':
+        return <input type="checkbox"/>;
+      case 'radio':
+        return <input type="radio"/>;
+      case 'text':
+        return <input type="text"/>;
+      case 'textarea':
+        return <textarea />;
+      default:
+        throw 'unknown cellType: ' + cellType;
+    }
   }
   renderBody() {
-    const { vChoices, hChoices } = this.props;
+    const { questions, values, direction, cellType } = this.props;
+    const rowLabels = this.isHorizontal() ? questions : values;
+    const columnLabels = this.isHorizontal() ? values : questions;
     return <tbody>
-      {vChoices.map(v => (
-        <tr>
-          <td>{v}</td>
-          {hChoices.map(h => <td><input type="checkbox"/></td>)}
-        </tr>
-      ))}
+      {rowLabels.map((v, vIndex) => {
+        let prefix = '', postfix = '', label = '';
+        if (!this.isHorizontal() && !Utils.isString(v)) {
+          prefix = v.prefix ? <span className="prefix">{v.prefix}</span> : '';
+          postfix = v.postfix ? <span className="postfix">{v.postfix}</span> : '';
+          label = v.label;
+        } else {
+          label = v;
+        }
+        return (
+          <tr>
+            <td>{label}</td>
+            {columnLabels.map((h, hIndex) => {
+              if (this.isHorizontal() && !Utils.isString(h)) {
+                prefix = h.prefix ? <span className="prefix">{h.prefix}</span> : '';
+                postfix = h.postfix ? <span className="postfix">{h.postfix}</span> : '';
+              }
+              return <td>{prefix}{this.renderCell()}{postfix}</td>
+            })}
+          </tr>
+        );
+      })}
       </tbody>;
   }
   render() {
-    const { vChoices, hChoices } = this.props;
+    const { questions, values, direction, cellType } = this.props;
+    if (!questions) {
+      return Utils.errorMessage('questions attribute is not defined');
+    }
+    if (!values) {
+      return Utils.errorMessage('values attribute is not defined');
+    }
+    if (!(direction === 'horizontal' || direction === 'vertical')) {
+      return Utils.errorMessage(`direction attribute must be 'horizontal' or 'vertical'`);
+    }
+    const cellTypes = ['checkbox', 'radio', 'text', 'textarea'];
+    if (!cellTypes.includes(cellType)) {
+      return Utils.errorMessage(`cellType attribute must be {cellTypes.join(' or ')}`);
+    }
     return (
       <div className={this.constructor.name}>
         <table>
@@ -54,12 +117,12 @@ export default class MatrixQuestion extends Component {
 }
 
 MatrixQuestion.defaultProps = {
-  values: [],
-  vertical: true
+  cellType: 'radio',
+  direction: 'horizontal'
 };
 
 MatrixQuestion.propTypes = {
   type: PropTypes.string.isRequired,
-  vChoices: PropTypes.array.isRequired,
-  hChoices: PropTypes.array.isRequired,
+  questions: PropTypes.array.isRequired,
+  values: PropTypes.array.isRequired,
 };
