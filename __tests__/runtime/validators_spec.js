f9c6eb7c883eaf2d1594f18b43f7ede1c015c9b4
Jiro Iwamoto <jiro-iwamoto@m3.com>
2017-02-01 19:02:42 +0900

    CheckboxQuestionの作成完了

--- __tests__/runtime/validators_spec.js
+++ __tests__/runtime/validators_spec.js
@@ -2,7 +2,7 @@
 import validate from '../../lib/runtime/validators';
 
 describe('Validator', () => {
-  describe('validateCheckboxQuestion', () => {
+  describe('CheckboxQuestion', () => {
     it('チェックボックスのチェック数がminCheckCountに満たない場合エラーが返る', () => {
       const value = {
         123: [
@@ -62,8 +62,36 @@ describe('Validator', () => {
       const constraints = {
         123: { CheckboxQuestion: { maxCheckCount: 2 } },
       };
-      const result = validate(value, constraints);
+      const result = validate(value, constraints, { fullMessages: false });
       expect(result).toBe(undefined);
+    });
+
+    it('input:textがある場合には入力必須', () => {
+      const value = {
+        123: [
+          { checked: true, freeText: '' },
+        ],
+      };
+      const constraints = {
+        123: { CheckboxQuestion: { minCheckCount: 1 } },
+      };
+      const result = validate(value, constraints, { fullMessages: false });
+      expect(result[123].length).toBe(1);
+      expect(result[123][0]).toBe('テキスト欄に入力してください');
+    });
+
+    it('input:numberがある場合には入力必須', () => {
+      const value = {
+        123: [
+          { checked: true, freeNumber: '' },
+        ],
+      };
+      const constraints = {
+        123: { CheckboxQuestion: { minCheckCount: 1 } },
+      };
+      const result = validate(value, constraints, { fullMessages: false });
+      expect(result[123].length).toBe(1);
+      expect(result[123][0]).toBe('テキスト欄に数値を入力してください');
     });
   });
 });
