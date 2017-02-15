134c0f299f8bc4b028eb026b2de2de709d679fbe
Jiro Iwamoto <jiro-iwamoto@m3.com>
2017-02-15 14:56:33 +0900

    不要なフォルダの削除

--- example-project/src/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env browser,jquery */
-import Editor from 'survey-designer-js/dist/editor.bundle';
-
-$.getJSON('sample.json').done((json) => {
-  const rootElement = document.getElementById('root');
-  Editor(rootElement, json);
-});
