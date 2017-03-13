199b22e595562ce95108d7d5a1077b8fc656277e
Jiro Iwamoto <jiro-iwamoto@m3.com>
2017-03-13 17:45:47 +0900

    Bootstrapのvariablesを読み込むように修正

--- webpack.config.js
+++ webpack.config.js
@@ -58,6 +58,14 @@ module.exports = {
         test: /\.scss$/,
         loader: 'style!css!sass',
       },
+      {
+        test: /\.less$/,
+        loader: 'style!css!less',
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
+      },
     ],
   },
   eslint: {
