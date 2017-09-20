e27eb4221fda64209a79ccc06eb31b7a22c68969
Jiro Iwamoto <jiro-iwamoto@m3.com>
2017-09-20 18:07:45 +0900

    全角数字が入力された場合ページ内再掲が正しく動かない不具合を修正

--- __tests__/runtime/models/survey/LogicalVariableDefinition_spec.js
+++ __tests__/runtime/models/survey/LogicalVariableDefinition_spec.js
@@ -9,7 +9,7 @@ describe('LogicalVariableDefinition', () => {
       const page = survey.getPages().get(0);
       const logicalVariable = page.getLogicalVariables().get(0);
       const result = logicalVariable.createFunctionCode(survey, page);
-      expect(result).toBe(`return (parseInt($('*[name=\"cj6u330kl00063j681zt4zvz1__value1\"]:enabled:visible').val(), 10) || 0)+(parseInt($('*[name=\"cj6u330kl00063j681zt4zvz1__value1\"]:enabled:visible').val(), 10) || 0)`);
+      expect(result).toBe("return (parseInt(($('*[name=\"cj6u330kl00063j681zt4zvz1__value1\"]:enabled:visible').val() || '').replace(/[０-９]/g, function(ch) { return String.fromCharCode(ch.charCodeAt(0) - 65248); }), 10) || 0)+(parseInt(($('*[name=\"cj6u330kl00063j681zt4zvz1__value1\"]:enabled:visible').val() || '').replace(/[０-９]/g, function(ch) { return String.fromCharCode(ch.charCodeAt(0) - 65248); }), 10) || 0)");
     });
 
     it('前のページの数値記入とロジック変数と現在のページの数値記入の足し算の場合', () => {
@@ -17,7 +17,7 @@ describe('LogicalVariableDefinition', () => {
       const page = survey.getPages().get(1);
       const logicalVariable = page.getLogicalVariables().get(0);
       const result = logicalVariable.createFunctionCode(survey, page);
-      expect(result).toBe(`return (parseInt(answers['cj6u330kl00063j681zt4zvz1__value1'], 10) || 0)+(parseInt(answers['cj6u339cf000i3j68ir0kwb37'], 10) || 0)+(parseInt($('*[name=\"cj6u333tl000c3j686m1gkv3w__value1\"]:enabled:visible').val(), 10) || 0)`);
+      expect(result).toBe("return (parseInt(answers['cj6u330kl00063j681zt4zvz1__value1'], 10) || 0)+(parseInt(answers['cj6u339cf000i3j68ir0kwb37'], 10) || 0)+(parseInt(($('*[name=\"cj6u333tl000c3j686m1gkv3w__value1\"]:enabled:visible').val() || '').replace(/[０-９]/g, function(ch) { return String.fromCharCode(ch.charCodeAt(0) - 65248); }), 10) || 0)");
     });
   });
 });
