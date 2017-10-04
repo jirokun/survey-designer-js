2d288ec31a39af7584967deb5eb5633be732e8ef
Jiro Iwamoto <jiro-iwamoto@m3.com>
2017-10-04 12:27:51 +0900

    失敗しているテストの修正

--- __tests__/runtime/models/options/Options_spec.js
+++ __tests__/runtime/models/options/Options_spec.js
@@ -6,9 +6,8 @@ import CssOption from '../../../../lib/runtime/models/options/CssOption';
 describe('Option', () => {
   describe('getShowDetailUrl', () => {
     it('詳細プレビューのURLを取得できる', () => {
-      const option = new Option({showDetailUrl: 'http://example.com'});
-      expect(option.getShowDetailUrl(false)).toBe('http://example.com');
-      expect(option.getShowDetailUrl(true)).toBe('http://example.com?env=development');
+      const option = new Option({ showDetailUrl: 'http://example.com' });
+      expect(option.getShowDetailUrl()).toBe('http://example.com');
     });
   });
 
